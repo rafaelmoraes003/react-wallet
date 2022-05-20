f13cb1bff8ef42446b9af467814d02064197d0f4
rafael <rafaelsm003@gmail.com>
2022-05-20 17:10:12 -0300

    fix: requisito 8

--- src/components/TableExpenses.js
+++ src/components/TableExpenses.js
@@ -7,40 +7,46 @@ class TableExpenses extends React.Component {
     const { expenses } = this.props;
     return (
       <table>
-        <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </tr>
+        <tbody>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </tbody>
 
-        {expenses.length > 0 && (
-          expenses.map((expense) => (
-            <tr key={ expense.description }>
-              <td>{expense.description}</td>
-              <td>{expense.tag}</td>
-              <td>{expense.method}</td>
-              <td>{Number(expense.value).toFixed(2)}</td>
-              <td>{expense.exchangeRates[expense.currency].name.split('/')[0]}</td>
-              <td>
-                {
-                  Number(expense.exchangeRates[expense.currency].ask).toFixed(2)
-                }
-              </td>
-              <td>
-                {
-                  (expense.value * expense.exchangeRates[expense.currency].ask).toFixed(2)
-                }
-              </td>
-              <td>Real</td>
-            </tr>
-          ))
-        )}
+        <tbody>
+          {expenses.length > 0 && (
+            expenses.map((expense) => (
+              <tr key={ expense.description }>
+                <td>{expense.description}</td>
+                <td>{expense.tag}</td>
+                <td>{expense.method}</td>
+                <td>{Number(expense.value).toFixed(2)}</td>
+                <td>{expense.exchangeRates[expense.currency].name.split('/')[0]}</td>
+                <td>
+                  {
+                    Number(expense.exchangeRates[expense.currency].ask).toFixed(2)
+                  }
+                </td>
+                <td>
+                  {
+                    (expense.value * expense.exchangeRates[expense.currency].ask)
+                      .toFixed(2)
+                  }
+                </td>
+                <td>Real</td>
+                <td><button type="button" data-testid="delete-btn">Excluir</button></td>
+              </tr>
+            ))
+          )}
+        </tbody>
       </table>
     );
   }
